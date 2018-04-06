5e62d339f99b606957a3c97e0152cdeece03ae01
Eric Chrobak <eric@ericchrobak.com>
2018-04-06 01:55:10 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+def display_board(board)
+  print board
+end
+
+def valid_move?(board, index)
+  if position_taken? || index > 8
+    return false
+  else
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def input_to_index(index)
+  if index == "" || index == " " || index == nil
+    return -1
+  else
+    index.to_i
+  end
+end
+
+def move(board, index, character = x)
+  index -= 1
+  input_to_index(index)
+  valid_move(board, index)
+  board[index] = character
+  return board
+end
